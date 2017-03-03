cc10d19d46d2d1158cef03b104a9927b01571c91
gallayl <lajos.gallay@digicpictures.com>
2017-03-03 16:35:19 +0100

    [WIP] Builder

--- src/EndpointModel/Builder.ts
+++ src/EndpointModel/Builder.ts
@@ -5,7 +5,8 @@ export class Builder {
     private EntitySets: {}[]
 
     public EntitySet<T extends IODataEntity, T2>(entityTypeClass: {new():T}, entitySetName:string):Builder{
-
+        //ToDo: CheckMe
+        // https://www.typescriptlang.org/docs/handbook/generics.html
         console.log(entityTypeClass.prototype);
         return this;
     }
