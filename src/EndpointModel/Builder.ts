355635cc28ba02c6a9972ddfaafad27194a69661
gallayl <lajos.gallay@digicpictures.com>
2017-03-06 13:29:57 +0100

    Fix: builder.EntitySet<T>

--- src/EndpointModel/Builder.ts
+++ src/EndpointModel/Builder.ts
@@ -7,12 +7,12 @@ export class Builder {
     private EntitySets: EntitySet[] = [];
 
     public EntityType<K extends keyof T, T>(entityTypeClass: { new (): T }, keyfield?: K | K[]): EntityType {
-        let existing = this.EntityTypes.find(t=>t.Name == entityTypeClass.name);
-        if (existing){
+        let existing = this.EntityTypes.find(t => t.Name == entityTypeClass.name);
+        if (existing) {
             return existing;
         }
 
-        if (!keyfield){
+        if (!keyfield) {
             throw new Error(`Key field(s) are required on the first definition for EntityType '${entityTypeClass.name}'`);
         }
 
@@ -22,24 +22,27 @@ export class Builder {
     }
 
     public EntitySet<T>(entityTypeClass: { new (): T }, entitySetName: string): EntitySet {
-        
-        let existing = this.EntitySets.find(s=>s.CollectionName == entitySetName);
+
+        let existing = this.EntitySets.find(s => s.CollectionName == entitySetName);
         if (existing) {
-            if (existing.EntityType.Name != entityTypeClass.name){
-                throw new Error(`Mismatch on registering entitySet '${entitySetName}', with type '${entityTypeClass.name}. Already registered to type '${existing.EntityType.Name}'`)
+            if (existing.EntityType.Name != entityTypeClass.name) {
+                throw new Error(`Mismatch on registering entitySet '${entitySetName}', with type '${entityTypeClass.name}. 
+                Already registered to type '${existing.EntityType.Name}'`)
             }
             return existing;
         }
 
-        let entityType = this.EntityTypes.find(e=>e.Name == entityTypeClass.name);
-        if (!entityType){
+        let entityType = this.EntityTypes.find(e => e.Name == entityTypeClass.name);
+        if (!entityType) {
             throw new Error(`Entity type not yet added for type '${entityTypeClass.name}', please add it first.`)
         }
 
-        let newEntitySet : EntitySet = {
+        let newEntitySet: EntitySet = {
             CollectionName: entitySetName,
             EntityType: entityType
         };
+
+        this.EntitySets.push(newEntitySet);
 
         return newEntitySet;
     }
