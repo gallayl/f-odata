26608192609fd853f837f18f38323771ed08d5bf
Gallay Lajos <Gallay Lajos>
2017-03-31 21:37:24 +0200

    fix: edm lint

--- xmlns/xml-primitives.js
+++ xmlns/xml-primitives.js
@@ -2,17 +2,17 @@ var cxml = require('cxml');
 
 cxml.register('xml-primitives', exports, [
 ], [
-	'any',
-	'boolean',
-	'Date',
-	'number',
-	'string'
+'any',
+'boolean',
+'Date',
+'number',
+'string',
 ], [
-	[0, 0, [], []],
-	[3, 1, [], []],
-	[3, 2, [], []],
-	[3, 3, [], []],
-	[3, 4, [], []],
-	[3, 5, [], []]
+[0, 0, [], []],
+[3, 1, [], []],
+[3, 2, [], []],
+[3, 3, [], []],
+[3, 4, [], []],
+[3, 5, [], []],
 ], [
 ]);
